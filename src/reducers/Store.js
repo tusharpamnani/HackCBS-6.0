666bffe3809f44459691281feaff2945e3d455b2
Tushar Pamnani <121151091+tusharpamnani@users.noreply.github.com>
2023-11-05 13:41:08 +0530

    Delete src directory

--- src/reducers/Store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import UserDataSlice from "./UserDataSlice";
-
-export  const store = configureStore({
-    name: "",
-    reducer: {
-        businessFormReducer: UserDataSlice,
-    }
-})
